46b036a7403f3c8353272f4a87840d087628c192
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-28 17:27:47 +0200

    ok

--- examples/halt.rs
+++ examples/halt.rs
@@ -1,3 +1,17 @@
+use ssh2::Session;
+use std::env;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Shutdown nas");
+    // Connect to the local SSH server
+    let tcp = TcpStream::connect("192.168.1.123:22").unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+
+    let args: Vec<String> = env::args().collect();
+
+    let login = &args[1];
+    let password = &args[2];
+    sess.userauth_password(login, password).unwrap();
 }
